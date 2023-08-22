41504c139ad61c43d3138dd438cc5a7bd154552b
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2023-08-22 17:01:14 -0500

    Partially decompile sub_8974

--- src/pokedex_end.c
+++ src/pokedex_end.c
@@ -3,13 +3,46 @@
 
 extern s16 gUnknown_0202A1C0[];
 extern s16 gUnknown_0202A588;
+extern s32 gUnknown_0202BF14;
+
 extern s16 gUnknown_086A61BC[];
 
-s16 sub_8A78(s32 unk_a)
+/* TODO unfinished
+void sub_8974(s32 arg0)
 {
-    s16 a = (s16) unk_a;
+    s32 var0 = gUnknown_086A61BC[(s16) arg0];
+    s32 quotient;
+    s16 remainder;
+    s16 var1;
     
-    if ((gUnknown_0202A1C0[a] == 4) && (gUnknown_086A61BC[a] != -1))
+    if (var0 != -1)
+    {
+        if (var0 < 100)
+        {
+            gUnknown_0202BF14 = 0;
+            quotient = var0 / 5;
+            remainder = var0 % 5;
+            var1 = (s16) var0;
+            
+            sub_10708(___ + remainder * 0xD80, (void *)(OBJ_VRAM0 + 0x3800), 0x6C, 1);
+        }
+        else
+        {
+            gUnknown_0202BF14 = 1;
+            quotient = (var0 - 100) / 6;
+            remainder = (var0 - 100) / 6;
+            var1 = (s16) var0;
+            
+            sub_10708((void *)(gMonHatchSpriteGroupGfx[var1] + remainder * 0x10E0), (void *)(OBJ_VRAM0 + 0x4800), 0x87, 1);
+        }
+    }
+}*/
+
+s16 sub_8A78(s32 arg0)
+{
+    s16 var0 = (s16) arg0;
+    
+    if ((gUnknown_0202A1C0[var0] == 4) && (gUnknown_086A61BC[var0] != -1))
     {
         gUnknown_0202A588 = 1;
     }
