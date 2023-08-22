c2e52a7c47ccc73cd471a96e8a5d4be3b4af03cf
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2023-08-22 16:23:46 -0500

    Decompile sub_8A78

--- src/pokedex_end.c
+++ src/pokedex_end.c
@@ -1,6 +1,26 @@
 #include "global.h"
 #include "main.h"
 
+extern s16 gUnknown_0202A1C0[];
+extern s16 gUnknown_0202A588;
+extern s16 gUnknown_086A61BC[];
+
+s16 sub_8A78(s32 unk_a)
+{
+    s16 a = (s16) unk_a;
+    
+    if ((gUnknown_0202A1C0[a] == 4) && (gUnknown_086A61BC[a] != -1))
+    {
+        gUnknown_0202A588 = 1;
+    }
+    else
+    {
+        gUnknown_0202A588 = 0;
+    }
+    
+    return gUnknown_0202A588;
+}
+
 void ResetPokedex(void)
 {
     s32 i;
