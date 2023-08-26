0df3e2ff9d45b1d6f60e15bd36dc5d4789a5f182
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2023-08-26 18:51:29 -0500

    Decompile sub_3FAC

--- src/pokedex.c
+++ src/pokedex.c
@@ -20,6 +20,41 @@ extern u16 gUnknown_08089760[];
 extern u16 gUnknown_08089960[];
 extern u16 gUnknown_08089B60[];
 
+extern s16 gUnknown_0201A448;
+extern s16 gUnknown_0202BF00;
+extern s16 gUnknown_0202A57C;
+extern s16 gUnknown_0201A4F0;
+extern s16 gUnknown_0202BE20;
+extern s16 gUnknown_0202C58C;
+extern s8 gUnknown_02019C24;
+extern s16 gUnknown_0202BF0C;
+extern s16 gUnknown_0201A440;
+extern s16 gUnknown_0202BEF4;
+extern s16 gUnknown_0202A588;
+extern s16 gUnknown_0202A55C;
+extern s16 gUnknown_0202A568[2];
+extern s16 gUnknown_0202C5E8;
+extern s16 gUnknown_0202A558;
+extern s8 gUnknown_0202BEE0;
+extern s8 gUnknown_0202BF04;
+extern s16 gUnknown_0202BF14;
+extern s8 gUnknown_0202C590;
+extern s16 gUnknown_0201B120;
+extern s8 gUnknown_0202BEC4;
+extern s8 gUnknown_0202BEFC;
+extern s8 gUnknown_02002830;
+extern s8 gUnknown_02002831;
+extern s8 gUnknown_0202C794;
+extern s8 gUnknown_0201C1B4;
+extern s8 gUnknown_0202C5AC;
+extern s8 gUnknown_02019C28;
+extern s16 gUnknown_0202A1C0[];
+extern s16 gUnknown_0202A390[];
+extern s16 gUnknown_0202A574;
+
+void sub_88E4(void);
+s8 sub_FD20(void);
+
 void PokedexMain(void)
 {
     gPokedexStateFuncs[gMain.subState]();
@@ -70,4 +105,82 @@ void LoadPokedexGraphics(void)
     gMain.subState = 1;
     
     m4aSongNumStart(MUS_POKEDEX);
+}
+
+void sub_3FAC(void) {
+    s32 i;
+    
+    gUnknown_0202ADE0 = 0;
+    gUnknown_0202C5B0 = 0;
+    gUnknown_0201A448 = 0;
+    gUnknown_0202BF00 = 0;
+    gUnknown_0202A57C = 0;
+    gUnknown_0201A4F0 = 0;
+    gUnknown_0202BE20 = 0;
+    gUnknown_0202C58C = 0;
+    gUnknown_02019C24 = 0;
+    gUnknown_0202BF0C = 0;
+    gUnknown_0201A440 = 0;
+    gUnknown_0202C5E8 = 0;
+    gUnknown_0202BEF4 = 0;
+    gUnknown_0202A588 = 1;
+    gUnknown_0202A55C = 1;
+    
+    gUnknown_0202A568[0] = 0;
+    gUnknown_0202A568[1] = 0;
+    
+    gUnknown_0202BF14 = 0;
+    gUnknown_0202A558 = 0;
+    gUnknown_0202BEE0 = 0;
+    gUnknown_0202BF04 = 1;
+
+    sub_88E4();
+    if (sub_FD20() == 1)
+    {
+        gUnknown_0202C590 = 1;
+    }
+    else
+    {
+        gUnknown_0202C590 = 0;
+    }
+
+    gUnknown_0202BEC4 = 0;
+    gUnknown_0202BEFC = 0;
+    gUnknown_0201B120 = 0;
+    gUnknown_02002830 = 0;
+    gUnknown_02002831 = 0;
+    gUnknown_0202C794 = 0;
+    gUnknown_0201C1B4 = 0;
+    gUnknown_0202C5AC = 0;
+    gUnknown_02019C28 = 0;
+
+    for (i = 0; i < 0xE1; i++)
+    {
+        gUnknown_0202A390[i] = gUnknown_0202A1C0[i];
+    }
+
+    if (gUnknown_0202A1C0[SPECIES_AERODACTYL])
+    {
+        gUnknown_0202A574 = SPECIES_AERODACTYL + 1;
+    }
+    else if (gUnknown_0202A1C0[SPECIES_TOTODILE])
+    {
+        gUnknown_0202A574 = SPECIES_TOTODILE + 1;
+    }
+    else if (gUnknown_0202A1C0[SPECIES_CYNDAQUIL])
+    {
+        gUnknown_0202A574 = SPECIES_CYNDAQUIL + 1;
+    }
+    else if (gUnknown_0202A1C0[SPECIES_CHIKORITA])
+    {
+        gUnknown_0202A574 = SPECIES_CHIKORITA + 1;
+    }
+    else if (gUnknown_0202A1C0[SPECIES_JIRACHI])
+    {
+        gUnknown_0202A574 = SPECIES_JIRACHI + 1;
+    }
+    else
+    {
+        gUnknown_0202A574 = SPECIES_RAYQUAZA + 1;
+    }
 }
