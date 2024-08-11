2629168ec62a54940a8662648cad2b28b7d7e747
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2024-08-11 15:35:13 -0500

    [TEMPLATE] Add link.c and link.h

---
+++ include/link.h
@@ -0,0 +1,19 @@
+// include guard
+
+// include macros
+
+// "extern" define macros
+
+// "extern" enums
+
+// "extern" struct definitions
+
+// extern function declarations
+
+// extern IWRAM variable declarations
+
+// extern EWRAM variable declarations
+
+// extern const definitions
+
+// end include guard
---
+++ src/link.c
@@ -0,0 +1,17 @@
+// include macros
+
+// "static" define macros
+
+// "static" enums
+
+// "static" struct definitions
+
+// static function declarations
+
+// static IWRAM variable declarations
+
+// static EWRAM variable declarations
+
+// static const definitions
+
+// function definitions
