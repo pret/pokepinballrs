a4365c9f2d51a2a21d1c726ec844e73e6fa586d1
Adewotta <dylanwestbro01@gmail.com>
2025-02-04 11:19:18 +0000

    Style

--- src/rom_11B9C.c
+++ src/rom_11B9C.c
@@ -6,15 +6,18 @@ void sub_11C14(s32);
 void sub_12524(void);
 void sub_12BF8(void);
 
-void sub_11B9C(void){
+void sub_11B9C(void)
+{
     s16 i;
-    if (gMain.unk6 == 0){
+    if (gMain.unk6 == 0)
+    {
         gCurrentPinballGame->unk132c = gCurrentPinballGame->unk1334[0];
         sub_11C14(0);
         sub_12524();
     }else{
         i = 0;
-        for (; i < 2; i++) {
+        for (; i < 2; i++)
+        {
             gCurrentPinballGame->unk132c = gCurrentPinballGame->unk1334[i];
             sub_11C14(i);
         }
