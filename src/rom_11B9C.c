0392d2c8349aada69a0e8eb0fcb3ed4ed502f64f
Adewotta <dylanwestbro01@gmail.com>
2025-02-06 02:02:35 +0000

    Fix silly merge mistake

--- src/rom_11B9C.c
+++ src/rom_11B9C.c
@@ -1,33 +1,6 @@
+#include "global.h"
 #include "constants/bg_music.h"
 #include "main.h"
-#include "global.h"
-
-void sub_11C14(s32);
-void sub_12524(void);
-void sub_12BF8(void);
-
-void sub_11B9C(void)
-{
-    s16 i;
-    if (gMain.unk6 == 0)
-    {
-        gCurrentPinballGame->unk132c = gCurrentPinballGame->unk1334[0];
-        sub_11C14(0);
-        sub_12524();
-    }
-    else
-    {
-        for (i = 0; i < 2; i++)
-        {
-            gCurrentPinballGame->unk132c = gCurrentPinballGame->unk1334[i];
-            sub_11C14(i);
-        }
-        sub_12BF8();
-    }
-}
-#include "constants/bg_music.h"
-#include "main.h"
-#include "global.h"
 
 void sub_11C14(s16);
 void sub_12524(void);
