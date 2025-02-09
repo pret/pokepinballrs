1a70ccc7edc8cc453f1c62b0db2cac87b681a261
Marcus Huderle <huderlem@gmail.com>
2025-02-09 16:18:28 -0600

    lil cleanup of sub_19D04

--- src/rom_19D04.c
+++ src/rom_19D04.c
@@ -4,16 +4,12 @@ extern void sub_21238(s32);
 
 void sub_19D04(void)
 {
-    if ((gCurrentPinballGame->unk13 == 2) && (2 < gCurrentPinballGame->unk14))
-    {
+    if (gCurrentPinballGame->unk13 == 2 && gCurrentPinballGame->unk14 > 2)
         sub_21238(0);
-    }
-    if (2 < gCurrentPinballGame->unk13)
-    {
-        return;
-    }
 
-    // Normal switch doesn't work... is it an enum switch?
+    if (gCurrentPinballGame->unk13 > 2)
+        return;
+
     if (gCurrentPinballGame->unk14 == 3)
     {
         gCurrentPinballGame->unk73C = gCurrentPinballGame->unk72E;
@@ -21,6 +17,7 @@ void sub_19D04(void)
         gCurrentPinballGame->unk72E = 0;
         gCurrentPinballGame->unk72F = 0;
     }
+
     if (gCurrentPinballGame->unk14 == 4)
     {
         gCurrentPinballGame->unk73C = gCurrentPinballGame->unk72E;
@@ -28,24 +25,18 @@ void sub_19D04(void)
         gCurrentPinballGame->unk72E = 0;
         gCurrentPinballGame->unk72F = 0;
     }
-    else
+    else if (gCurrentPinballGame->unk14 == 6)
     {
-        if (gCurrentPinballGame->unk14 == 6)
-        {
-            gCurrentPinballGame->catchModeArrows = gCurrentPinballGame->unk72F;
-            gCurrentPinballGame->unk73C = 0;
-            gCurrentPinballGame->unk72E = 0;
-            gCurrentPinballGame->unk72F = 0;
-            return;
-        }
-        if (gCurrentPinballGame->unk14 < 4)
-        {
-            return;
-        }
+        gCurrentPinballGame->catchModeArrows = gCurrentPinballGame->unk72F;
+        gCurrentPinballGame->unk73C = 0;
+        gCurrentPinballGame->unk72E = 0;
+        gCurrentPinballGame->unk72F = 0;
+    }
+    else if (gCurrentPinballGame->unk14 > 3)
+    {
         gCurrentPinballGame->unk73C = gCurrentPinballGame->unk72E;
         gCurrentPinballGame->catchModeArrows = gCurrentPinballGame->unk72F;
         gCurrentPinballGame->unk72E = 0;
         gCurrentPinballGame->unk72F = 0;
     }
-
 }
