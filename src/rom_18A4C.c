8553e378bf2f74d39729903c5b5a14fa29aefc68
Marcus Huderle <huderlem@gmail.com>
2025-05-30 16:10:39 -0500

    spaces

--- src/rom_18A4C.c
+++ src/rom_18A4C.c
@@ -36,25 +36,25 @@ void sub_18AE0(u8 unknown_enum)
     // input presumed to be an enum due to this switch case below
     // something told the compiler to make 15 cases not 0 based
     switch (unknown_enum)
-	{
-	case 4:
-		gCurrentPinballGame->unk3DC = 6;
-		gCurrentPinballGame->unk1F = 1;
-		break;
-	case 1:
-	case 2:
-	case 3:
-	case 5:
-	case 6:
-	case 7:
-	case 8:
-	case 9:
-	case 10:
-	case 11:
-	case 12:
-	case 13:
-	case 14:
-	case 15:
+    {
+    case 4:
+        gCurrentPinballGame->unk3DC = 6;
+        gCurrentPinballGame->unk1F = 1;
+        break;
+    case 1:
+    case 2:
+    case 3:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
         break;
     }
 }
