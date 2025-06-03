21ff4472d521782ef809181a85e997dbe85ed816
THCFree <me@thcfree.dev>
2025-06-03 08:31:42 +0200

    Decompile sub_17634

---
+++ src/rom_17634.c
@@ -0,0 +1,29 @@
+//
+// Created by Delta on 6/3/25.
+//
+#include "global.h"
+void sub_17634(u8 param)
+{
+    switch (param) {
+        case 1:
+        case 2:
+        case 3:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            break;
+        case 4:
+            gCurrentPinballGame->unk3DC = 6;
+            gCurrentPinballGame->unk1F = 1;
+            gCurrentPinballGame->unk387 = 0;
+            break;
+    }
+}
