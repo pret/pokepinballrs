6233fe7bbbd1c72fd0b11cabb3edcd25cc25c4be
Retnuhytnuob <Retnuhytnuob@yahoo.com>
2025-08-31 15:43:34 -0500

    restored cleanup on sub_14740 and sub_1493C

--- src/rom_14740.c
+++ src/rom_14740.c
@@ -8,46 +8,41 @@ extern u16 gUnknown_086ACE2C[][2];
 extern s8 gUnknown_086ACDB8[];
 
 
-u16 sub_14740(struct Vector16* arg0, u16* arg1) 
+u16 sub_14740(struct Vector16 *arg0, u16 *arg1)
 {
     struct Vector16 vec1;
-    u16 x;
-    u16 y;
+    u16 x, y;
     u16 res;
-    u16 temp_r2;
+    u16 var0;
 
     res = 0;
-    if (gCurrentPinballGame->unk5A4 != 2) 
+    if (gCurrentPinballGame->unk5A4 != 2)
     {
         vec1.x = arg0->x / 2 - gCurrentPinballGame->unkC4;
         vec1.y = arg0->y / 2 - gCurrentPinballGame->unkC6;
-        
         if ((vec1.y >= 0 && vec1.y < 48) && (vec1.x >= 0 && vec1.x < 48))
         {
             s32 ix = vec1.y * 48 + vec1.x;
-            temp_r2 = gUnknown_0839A28C[ix]; 
-    
-            if (temp_r2 & 0x80) 
+            var0 = gUnknown_0839A28C[ix]; 
+            if (var0 & 0x80) 
             {
                 gCurrentPinballGame->unk22 = 2;
                 gCurrentPinballGame->unk23 = 3;
-
                 //Todo: fakematch; used to swap register order
-                *arg1 = (temp_r2 & 0x7F & temp_r2 ) * 512;
+                *arg1 = (var0 & 0x7F & var0) * 512;
                 gCurrentPinballGame->unk5A6 = 20;
-                
-                if (gCurrentPinballGame->unk5A9 != 0)
-                    gCurrentPinballGame->unk5A9 =24;
+
+                if (gCurrentPinballGame->unk5A9)
+                    gCurrentPinballGame->unk5A9 = 24;
                 else
                     gCurrentPinballGame->unk5A9 = 20;
-                
-                //Only difference between the two branches is the sound played
-                if (gCurrentPinballGame->currentSpecies == 200) 
+
+                // Only difference between these two branches is the played sound
+                if (gCurrentPinballGame->currentSpecies == SPECIES_JIRACHI)
                 {
                     if (gCurrentPinballGame->unk5AA == 0) 
                     {
                         gCurrentPinballGame->unk5A5++;
-                        
                         if (gCurrentPinballGame->unk5A5 == 3) 
                         {
                             gCurrentPinballGame->unk71D[gCurrentPinballGame->unk5A5 - 1] = 3;
@@ -61,39 +56,36 @@ u16 sub_14740(struct Vector16* arg0, u16* arg1)
                             gCurrentPinballGame->unk5A4 = 1;
                             gCurrentPinballGame->unk3C = 10000;
                         }
-                        
-                        gCurrentPinballGame->unk5AA = 4;
 
+                        gCurrentPinballGame->unk5AA = 4;
                     }
-                    
+
                     res = 1;
                     m4aSongNumStart(SE_UNKNOWN_0x145);
                     sub_11B0(7);
-                } 
-                else 
+                }
+                else
                 {
-                    if (gCurrentPinballGame->unk5AA == 0) 
+                    if (gCurrentPinballGame->unk5AA == 0)
                     {
                         gCurrentPinballGame->unk5A5++;
-                    
-                        if (gCurrentPinballGame->unk5A5 == 3) 
+                        if (gCurrentPinballGame->unk5A5 == 3)
                         {
                             gCurrentPinballGame->unk71D[gCurrentPinballGame->unk5A5 - 1] = 3;
                             gCurrentPinballGame->unk5A4 = 2;
                             gCurrentPinballGame->unk5A6 = 0;
                             gCurrentPinballGame->unk3C = 10000;
-                        
-                        } 
-                        else 
+                        }
+                        else
                         {
                             gCurrentPinballGame->unk71D[gCurrentPinballGame->unk5A5 - 1] = 3;
                             gCurrentPinballGame->unk5A4 = 1;
                             gCurrentPinballGame->unk3C = 10000;
                         }
-                        
+
                         gCurrentPinballGame->unk5AA = 4;
                     }
-                    
+
                     res = 1;
                     m4aSongNumStart(SE_UNKNOWN_0x74);
                     sub_11B0(7);
@@ -101,76 +93,71 @@ u16 sub_14740(struct Vector16* arg0, u16* arg1)
             }
         }
     }
+
     return res;
 }
 
 
-void sub_1493C(void) {
+void sub_1493C(void)
+{
     struct Vector16 vec1;
-    struct Vector32 vec2;
-    
-    s32 mag1;
-    u16 tan1;
+    struct Vector32 vec2;    
+    int squaredMagnitude;
+    u16 angle;
     u8 temp_adjust;
 
     vec1.x = gCurrentPinballGame->ball->positionQ1.x - 238;
     vec1.y = gCurrentPinballGame->ball->positionQ1.y - 558;
-    mag1 = (vec1.x * vec1.x) + (vec1.y * vec1.y);
-    
-    if ((mag1 <= 1763) && !(1 & gCurrentPinballGame->unk1E)) 
+    squaredMagnitude = (vec1.x * vec1.x) + (vec1.y * vec1.y);
+
+    if (squaredMagnitude < 1764 && (gCurrentPinballGame->unk1E & 1) == 0) 
     {
-        tan1 = ArcTan2(-vec1.x, vec1.y);
-        
+        angle = ArcTan2(-vec1.x, vec1.y);
         temp_adjust = 30;
-        vec2.x = (temp_adjust  * Cos(tan1)) / 20000;
-        vec2.y = (-temp_adjust * Sin(tan1)) / 20000;
-        
+        vec2.x = (temp_adjust  * Cos(angle)) / 20000;
+        vec2.y = (-temp_adjust * Sin(angle)) / 20000;
         gCurrentPinballGame->ball->velocity.x = ((vec2.x * 100) + (98 * gCurrentPinballGame->ball->velocity.x)) / 100;
         gCurrentPinballGame->ball->velocity.y = ((vec2.y * 100) + (98 * gCurrentPinballGame->ball->velocity.y)) / 100;
     }
-    
-    if (gCurrentPinballGame->unk5F4 <= 19) 
+
+    if (gCurrentPinballGame->unk5F4 < 20)
     {    
-        if (mag1 <= 39) 
+        if (squaredMagnitude < 40) 
         {
-            gCurrentPinballGame->unk5F4 += 1;
+            gCurrentPinballGame->unk5F4++;
             gCurrentPinballGame->ball->unkE = 256;
         }
         else
         {
             gCurrentPinballGame->ball->unkE = 256;
-            
-            if (mag1 > 100)
+            if (squaredMagnitude > 100)
                 gCurrentPinballGame->unk5F4 = 0;
         }
     } 
     else 
     {
-        if (gCurrentPinballGame->unk5F4 <= 29) 
+        if (gCurrentPinballGame->unk5F4 < 30) 
         {
-            gCurrentPinballGame->unk5F4 += 1;
+            gCurrentPinballGame->unk5F4++;
             gCurrentPinballGame->ball->unkE = ((30 - gCurrentPinballGame->unk5F4) * 128) / 10 + 128;
-            
             gCurrentPinballGame->ball->positionQ0.x = 119;
             gCurrentPinballGame->ball->positionQ0.y = 279;
             gCurrentPinballGame->ball->unk6 = 0;
-            
             gCurrentPinballGame->ball->positionQ8.x = gCurrentPinballGame->ball->positionQ0.x << 8;
             gCurrentPinballGame->ball->positionQ8.y = gCurrentPinballGame->ball->positionQ0.y << 8;
-            
-            return;
         }
-
-        gCurrentPinballGame->unk5F4 = 0;
-        gCurrentPinballGame->unk25 = 4;
-        
-        if (gMain.selectedField == 0)
-            sub_1A98C();
         else
-            sub_32914();
-        
-        gCurrentPinballGame->unk22 = 7;
-        gCurrentPinballGame->ball->unkE = 128;
+        {
+            gCurrentPinballGame->unk5F4 = 0;
+            gCurrentPinballGame->unk25 = 4;
+            if (gMain.selectedField == FIELD_RUBY)
+                sub_1A98C();
+            else
+                sub_32914();
+
+            gCurrentPinballGame->unk22 = 7;
+            gCurrentPinballGame->ball->unkE = 128;
+        }
     }
 }
 
