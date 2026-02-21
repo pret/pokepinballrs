00b481a0b02596d0daec8b30d488e4c7ff8894b3
Retnuhytnuob <65794339+Retnuhytnuob@users.noreply.github.com>
2026-02-21 10:35:27 -0600

    Dump Evolution item pickup, initial sprites (#198)
    * Evo appear gfx
    
    * leafstone
    
    * all but mismatched sun
    
    * sun alt appear/sparkle
    
    * update list name

--- data/rom_1.s
+++ data/rom_1.s
@@ -1405,38 +1405,38 @@ gUnknown_08158284:: @ 0x08158284
 gUnknown_0815A6A4:: @ 0x0815A6A4
 	.incbin "baserom.gba", 0x15A6A4, 0x200
 
-gUnknown_0815A8A4:: @ 0x0815A8A4
-	.incbin "baserom.gba", 0x15A8A4, 0x1C20
+gEvoExAppear_Gfx:: @ 0x0815A8A4
+	.incbin "graphics/board_pickups/evo_item_ex.4bpp";
 
 gUnknown_0815C4C4:: @ 0x0815C4C4
 	.incbin "baserom.gba", 0x15C4C4, 0x200
 
-gUnknown_0815C6C4:: @ 0x0815C6C4
-	.incbin "baserom.gba", 0x15C6C4, 0x1C20
+gEvoLeafAppear_Gfx:: @ 0x0815C6C4
+	.incbin "graphics/board_pickups/evo_item_leaf.4bpp";
 
-gUnknown_0815E2E4:: @ 0x0815E2E4
-	.incbin "baserom.gba", 0x15E2E4, 0x1C20
+gEvoFireAppear_Gfx:: @ 0x0815E2E4
+	.incbin "graphics/board_pickups/evo_item_fire.4bpp";
 
-gUnknown_0815FF04:: @ 0x0815FF04
-	.incbin "baserom.gba", 0x15FF04, 0x1C20
+gEvoHeartAppear_Gfx:: @ 0x0815FF04
+	.incbin "graphics/board_pickups/evo_item_heart.4bpp";
 
-gUnknown_08161B24:: @ 0x08161B24
-	.incbin "baserom.gba", 0x161B24, 0x1C20
+gEvoLinkAppear_Gfx:: @ 0x08161B24
+	.incbin "graphics/board_pickups/evo_item_link.4bpp";
 
-gUnknown_08163744:: @ 0x08163744
-	.incbin "baserom.gba", 0x163744, 0x1C20
+gEvoMoonAppear_Gfx:: @ 0x08163744
+	.incbin "graphics/board_pickups/evo_item_moon.4bpp";
 
-gUnknown_08165364:: @ 0x08165364
-	.incbin "baserom.gba", 0x165364, 0x1C20
+gEvoWaterAppear_Gfx:: @ 0x08165364
+	.incbin "graphics/board_pickups/evo_item_water.4bpp";
 
-gUnknown_08166F84:: @ 0x08166F84
-	.incbin "baserom.gba", 0x166F84, 0x1C20
+gEvoBoxAppear_Gfx:: @ 0x08166F84
+	.incbin "graphics/board_pickups/evo_item_box.4bpp";
 
-gUnknown_08168BA4:: @ 0x08168BA4
-	.incbin "baserom.gba", 0x168BA4, 0x1C20
+gEvoSunAppear_Gfx:: @ 0x08168BA4
+	.incbin "graphics/board_pickups/evo_item_sun.4bpp";
 
-gUnknown_0816A7C4:: @ 0x0816A7C4
-	.incbin "baserom.gba", 0x16A7C4, 0x1C20
+gEvoBoltAppear_Gfx:: @ 0x0816A7C4
+	.incbin "graphics/board_pickups/evo_item_bolt.4bpp";
 
 gUnknown_0816C3E4:: @ 0x0816C3E4
 	.incbin "baserom.gba", 0x16C3E4, 0x42800
