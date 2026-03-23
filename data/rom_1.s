cec6e4ca052343b6cfcb39a6a22d8b487f777324
Retnuhytnuob <Retnuhytnuob@yahoo.com>
2026-03-22 22:05:02 -0500

    travel paint

--- data/rom_1.s
+++ data/rom_1.s
@@ -890,7 +890,8 @@ gSapphireShopSignPalettes:: @ 0x081BC984
 	.incbin "baserom.gba", 0x1BC984, 0x120
 
 gRubyBoardBonusGfx:: @ 0x081BCAA4
-	.incbin "baserom.gba", 0x1BCAA4, 0x1820
+	.incbin "graphics/stage/main/travel_paint.4bpp"
+	.space 0x120
 
 gRubyBoardBonusObjPalette:: @ 0x081BE2C4
 	.incbin "baserom.gba", 0x1BE2C4, 0x200
